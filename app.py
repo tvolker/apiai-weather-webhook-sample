1e4fc6df5b418a1e2b3bd0436beb2e0b132ad78f
tvolker <thomas.p.volker@gmail.com>
2016-11-28 12:41:33 +0200

    app.py
    Changed Fahrenheid to Celcius

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ def makeWebhookResult(data):
     item = channel.get('item')
     location = channel.get('location')
     units = channel.get('units')
+    units = 'd'
     if (location is None) or (item is None) or (units is None):
         return {}
 
@@ -76,7 +77,7 @@ def makeWebhookResult(data):
         return {}
 
     # print(json.dumps(item, indent=4))
-
+    
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
